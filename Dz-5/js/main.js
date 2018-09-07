b2b367355a65c389b77e8dcba4cf2fb589154a39
DenTheProgrammer <den4ikk007@gmail.com>
2018-09-08 02:49:32 +0300

    better selecting

--- Dz-5/js/main.js
+++ Dz-5/js/main.js
@@ -1,5 +1,5 @@
 var div;
-var board = document.querySelector("div.board");
+var board = document.querySelector(".board");
 var letters = [, "A", "B", "C", "D", "E", "F", "G", "H"];
 
 
@@ -25,7 +25,7 @@ for (var i = 0; i < 8; i++) {
 	}
 }
 
-var cells = document.getElementsByClassName("cell");
+var cells = document.querySelectorAll(".cell");
 var c = 0;
 for (i = 1; i < 9; i++) {
 	for (j = 1; j < 9; j++) {
