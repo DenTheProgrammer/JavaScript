855242499c6320bcd5ed166677e77c0ca3f3e121
DenTheProgrammer <den4ikk007@gmail.com>
2018-09-08 01:56:19 +0300

    Cell-colors and cell-numbers

--- Dz-5/js/main.js
+++ Dz-5/js/main.js
@@ -1,7 +1,32 @@
 var div;
 var board = document.querySelector("div.board");
-for (var i = 0; i < 64; i++) {
-	div = document.createElement("div");
-	div.className = "cell";
-	board.appendChild(div);
+var letters = [, "A", "B", "C", "D", "E", "F", "G", "H"];
+
+
+for (var i = 0; i < 8; i++) {
+	for (var j = 0; j < 4; j++) {
+		if (i % 2 == 0) {
+			div = document.createElement("div");
+			div.classList.add("cell", "white");
+			board.appendChild(div);
+
+			div = document.createElement("div");
+			div.classList.add("cell", "black");
+			board.appendChild(div);
+		} else {
+			div = document.createElement("div");
+			div.classList.add("cell", "black");
+			board.appendChild(div);
+
+			div = document.createElement("div");
+			div.classList.add("cell", "white");
+			board.appendChild(div);
+		}
+	}
+}
+var k=3;
+for (i = 1; i < 9; i++) {
+	for (j = 1; j < 9; j++) {
+		board.childNodes[k++].classList.add(letters[i]+j);
+	}
 }
