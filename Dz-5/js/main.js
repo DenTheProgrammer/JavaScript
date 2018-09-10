e2203c74a9296a577d39397d87bbc522ed56bdf2
DenTheProgrammer <den4ikk007@gmail.com>
2018-09-10 23:27:29 +0300

    startPosition Done and bishop size fix

--- Dz-5/js/main.js
+++ Dz-5/js/main.js
@@ -29,14 +29,40 @@ var cells = document.querySelectorAll(".cell");
 var c = 0;
 for (i = 1; i < 9; i++) {
 	for (j = 1; j < 9; j++) {
-		cells[c++].classList.add(letters[i]+j);//добавляем класс с номером ячейки
+		cells[c++].classList.add(letters[i] + j); //добавляем класс с номером ячейки
 	}
 }
 
-//пешки
-for(i=1;i<9;i++){
-document.getElementsByClassName(letters[i]+2)[0].classList.add("whitePawn");
+
+function setStarnPosition() {
+	//пешки
+	for (i = 1; i < 9; i++) {
+		document.getElementsByClassName(letters[i] + 2)[0].classList.add("whitePawn");
+	}
+	for (i = 1; i < 9; i++) {
+		document.getElementsByClassName(letters[i] + 7)[0].classList.add("blackPawn");
+	}
+	//ладьи
+	document.querySelector(".A1").classList.add("whiteRook");
+	document.querySelector(".H1").classList.add("whiteRook");
+	document.querySelector(".A8").classList.add("blackRook");
+	document.querySelector(".H8").classList.add("blackRook");
+	//кони
+	document.querySelector(".B1").classList.add("whiteKnight");
+	document.querySelector(".G1").classList.add("whiteKnight");
+	document.querySelector(".B8").classList.add("blackKnight");
+	document.querySelector(".G8").classList.add("blackKnight");
+	//слоны
+	document.querySelector(".C1").classList.add("whiteBishop");
+	document.querySelector(".F1").classList.add("whiteBishop");
+	document.querySelector(".C8").classList.add("blackBishop");
+	document.querySelector(".F8").classList.add("blackBishop");
+	//королевы
+	document.querySelector(".D1").classList.add("whiteQueen");
+	document.querySelector(".D8").classList.add("blackQueen");
+	//короли
+	document.querySelector(".E1").classList.add("whiteKing");
+	document.querySelector(".E8").classList.add("blackKing");
 }
-for(i=1;i<9;i++){
-document.getElementsByClassName(letters[i]+7)[0].classList.add("blackPawn");
-}
+
+setStarnPosition();
