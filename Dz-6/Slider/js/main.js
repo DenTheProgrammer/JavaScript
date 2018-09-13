e62e8ac41a0864b544cf614d9a7bfe77bcbd2413
Den <den4ikk007@gmail.com>
2018-09-13 19:24:20 +0400

    added any img possibility

--- Dz-6/Slider/js/main.js
+++ Dz-6/Slider/js/main.js
@@ -4,25 +4,25 @@ autoSwitch();
 
 
 function autoSwitch() {
-	timer = setInterval(function () {
-			document.querySelector(".next").click();
-		},
-		7000);
+    timer = setInterval(function () {
+            document.querySelector(".next").click();
+        },
+        7000);
 }
 
-
+var imgNumber = document.querySelectorAll(".slider img").length;
 document.querySelector(".slider-container").onclick = function (e) {
-	if (e.target.classList[0] === "next") { //листаем вперед
-		active = (active) % 3 + 1;
-	} else if (e.target.classList[0] === "prev") { //назад
-		if (active > 1) {
-			active -= 1
-		} else {
-			active = 3;
-		}
-	}
-	document.querySelector(".active").classList.toggle("active");
-	document.querySelector("#img" + active).classList.toggle("active");
-	clearInterval(timer); //сброс таймера при клике пользователя
-	autoSwitch();
+    if (e.target.classList[0] === "next") { //листаем вперед
+        active = (active) % imgNumber + 1;
+    } else if (e.target.classList[0] === "prev") { //назад
+        if (active > 1) {
+            active -= 1
+        } else {
+            active = imgNumber;
+        }
+    }
+    document.querySelector(".active").classList.toggle("active");
+    document.querySelector("#img" + active).classList.toggle("active");
+    clearInterval(timer); //сброс таймера при клике пользователя
+    autoSwitch();
 }
