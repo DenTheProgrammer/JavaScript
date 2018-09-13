21a3d67a8bd44a81c640d5b1285fcb39a9d04ea4
DenTheProgrammer <den4ikk007@gmail.com>
2018-09-13 17:05:00 +0300

    modernized slider

---
+++ Dz-6/Slider/js/main.js
@@ -0,0 +1,28 @@
+var active = 1;
+var timer;
+autoSwitch();
+
+
+function autoSwitch() {
+	timer = setInterval(function () {
+			document.querySelector(".next").click();
+		},
+		7000);
+}
+
+
+document.querySelector(".slider-container").addEventListener("click", function (e) {
+	if (e.target.classList[0] === "next") { //листаем вперед
+		active = (active) % 3 + 1;
+	} else if (e.target.classList[0] === "prev") { //назад
+		if (active > 1) {
+			active -= 1
+		} else {
+			active = 3;
+		}
+	}
+	document.querySelector(".active").classList.toggle("active");
+	document.querySelector("#img" + active).classList.toggle("active");
+	clearInterval(timer); //сброс таймера при клике пользователя
+	autoSwitch();
+})
